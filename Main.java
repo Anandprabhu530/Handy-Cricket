a107105264828becbd1266fa184c221740595362
Anandprabhu530 <anandprabhu530@gmail.com>
2025-01-13 22:43:45 +0530

    Changes

--- Main.java
+++ Main.java
@@ -5,14 +5,26 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("-------------Welcome to Hand Cricket------------- \n\n");
         System.out.println("Choose anyone option below");
-        System.out.println("0: Single Player");
-        System.out.println("1: Multi-Player");
-        System.out.println("2: Instructions");
+        System.out.println("0: Single Player \n 1: Multi-Player \n 2: Instructions \n");
+        System.out.println("Enter your choice: ");
         int n = sc.nextInt();
-
+        System.out.println("----------------Toss----------------");
+        System.out.println("Odd or Even");
+        System.out.println("0: Odd \n 1: Even \n");
+        int tossChoosen = sc.nextInt();
+        boolean isEven = tossChoosen == 0 ? false : true;
+        System.out.print("Enter number between 1 - 6: ");
+        int choice = sc.nextInt();
+        System.out.println();
         switch (n) {
             case 0:
                 // Need to implement with a inbuilt function;
+                int tossResult = throwToss(isEven, choice, n);
+                if (tossResult == 1) {
+                    System.out.print("You won the toss");
+                } else {
+                    System.out.print("You loose the toss");
+                }
                 break;
             case 1:
                 // against two players in different terminals
@@ -26,7 +38,7 @@ public class Main {
         }
     }
     
-    public int throwToss(boolean isEven, int score, int choice) {
+    public static int throwToss(boolean isEven, int score, int choice) {
         if (choice == 0) {
             int computerNumber = (int) Math.floor(Math.random() * 6);
             // need to check once
@@ -36,7 +48,7 @@ public class Main {
                 return 0;
             }
         } else {
-            // need to implement to get both values from two different playerss
+            // need to implement to get both values from two different players
             return 0;
         }
     }
