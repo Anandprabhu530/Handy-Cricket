ad62c252dd3c230db33ef414da1b7cebec0a73fb
Anandprabhu530 <anandprabhu530@gmail.com>
2025-01-25 21:39:36 +0530

    Need to add threads

--- src/Room/Client/GameClient.java
+++ src/Room/Client/GameClient.java
@@ -4,6 +4,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class GameClient {
@@ -14,14 +15,25 @@ public class GameClient {
         int n = sc.nextInt();
 
         try {
-            Socket socket = new Socket("locahost", 6969);
+            Socket socket = new Socket("localhost", 6969);
             DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
             DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
 
-            byte[] arr = new byte[2];
-            arr[0] = n == 0 ? (byte) 0 : 1;
-            String temp = "radomcode";
-            
+            byte[] roomInput = new byte[2];
+
+            roomInput[0] = n == 0 ? (byte) 0 : 1;
+            if (n == 1) {
+                System.out.print("\nEnter room code: ");
+                int roomCode = sc.nextInt();
+                roomInput[1] = (byte) roomCode;
+            }
+            dataOutputStream.write(roomInput);
+            byte[] msg = new byte[2];
+
+            dataInputStream.read(msg);
+            System.out.println("msg[0]: " +msg[0] + "     msg[1]: " +msg[1]);
+
+            int nj = sc.nextInt();
         } catch (Exception e) {
             System.out.println("Unable to connect to server: " + e);
         }
--- src/Room/Server/GameServer.java
+++ src/Room/Server/GameServer.java
@@ -4,32 +4,61 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashSet;
 
 public class GameServer {
+    private static HashSet<Integer> set;
     public static void main(String[] args) {
         try {
             ServerSocket serverSocket = new ServerSocket(6969);
             Socket clientSocket = serverSocket.accept();
-
+            set = new HashSet<>();
+            System.out.println("Started");
             DataInputStream dataInputStream = new DataInputStream(clientSocket.getInputStream());
             DataOutputStream dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
 
-            int n = dataInputStream.readInt();
+            
 
+            byte[] response = new byte[2];
+            byte[] userChoice = new byte[2];
             //33, 126
-            StringBuilder roomcode = new StringBuilder();
-            if (n == 0) {
-                while (roomcode.length() != 6) {
-                    roomcode.append(roomcode);
+            while (true) {
+                System.out.println("Scanning");
+                dataInputStream.read(userChoice);
+                switch ((int) userChoice[0]) {
+                    case 0:
+                        int roomCode = roomCodeGenerator();
+                        while (set.contains(roomCode))
+                            roomCode = roomCodeGenerator();
+                        set.add(roomCode);
+                        response[0] = (byte) 0;
+                        response[1] = (byte) roomCode;
+                        System.out.println("Room code: " + roomCode);
+                        break;
+                    case 1:
+                        System.out.println(set);
+                        if (set.contains((int) userChoice[1])) {
+                            response[0] = (byte) 1;
+                            System.out.println("Found");
+                        } else {
+                            response[0] = (byte) -1;
+                            System.out.println("Not Found");
+                        }
+                        break;
                 }
+                System.out.println("User reached here");
+                dataOutputStream.write(response);
             }
+            
 
+            
         } catch (Exception e) {
             // TODO: handle exception
+            System.err.println("An error occured: " + e);
         }
     }
     
-    public static void randomString() {
-        char temp = 122 + '0';
+    public static int roomCodeGenerator() {
+        return (int) Math.floor(Math.random() * ((124-0) + 1));
     }
 }
